c15e276ed0faf92426274a0374f5d2d09f6a1fe9
hazycora <hazycora@riseup.net>
2023-02-06 19:01:28 -0600

    pad start of base16 track ids

--- src/utils/base62tohex.ts
+++ src/utils/base62tohex.ts
@@ -1,8 +1,9 @@
 export default function base62toHex(str: string): string {
 	const charset = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
 	const base = BigInt(62)
-	return str.split('').reverse().reduce(
+	let value = str.split('').reverse().reduce(
 		(prev, char, i) =>
-			prev + (BigInt(charset.indexOf(char)) * (base ** BigInt(i))),
-		BigInt(0)).toString(16)
+			prev + (BigInt(charset.indexOf(char)) * (base ** BigInt(i))
+		), BigInt(0)).toString(16)
+	return value.padStart(32, '0')
 }
