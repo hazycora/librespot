3021b3e7dd3f6377b716d63657acc4fec9d02770
uh wot <uhwot@protonmail.com>
2024-09-13 01:19:46 +0200

    fix track download example

--- docs/examples/download-track.ts
+++ docs/examples/download-track.ts
@@ -1,12 +1,10 @@
 import fs from 'fs'
 import Librespot from 'librespot'
 
-const spotify = new Librespot()
+const spotify = new Librespot({})
 
 await spotify.login("username", "password")
 
 const track = await spotify.get.track('1p80LdxRV74UKvL8gnD7ky')
 
 await fs.promises.writeFile('example.ogg', track.stream)
-
-await spotify.disconnect()
